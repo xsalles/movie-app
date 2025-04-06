00d420f658fe75900f34b3afeffcdb041900f2f4
xsalles <xs.salles@gmail.com>
2025-04-05 23:33:26 -0300

    feat: added bg to the home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, ScrollView } from "react-native";
+import React from "react";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 bg-primary w-full">
+      <Image source={images.bg} className="absolute w-full z-0" />
+
+      <ScrollView
+        className="flex-1 px-5 z-10"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
-  )
+  );
 }
